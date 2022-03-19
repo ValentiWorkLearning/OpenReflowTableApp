5fe0ad43fac6102667941eb04c48197d428a3526
Valentyn Korniienko <kornienko-vr@rambler.ru>
2022-03-19 16:59:41 +0200

    Fixed temperature request interval

--- reflow_process_controller/reflow_controller.cpp
+++ reflow_process_controller/reflow_controller.cpp
@@ -51,7 +51,7 @@ private:
     {
         using namespace std::chrono_literals;
         QTimer stateTimer;
-        stateTimer.setInterval(200ms);
+        stateTimer.setInterval(400ms);
         stateTimer.start();
         while (true)
         {
